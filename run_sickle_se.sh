070fff4e25a84a893b51dbcbfc793f5fbcbaa404
Sebastian Schmeier <s.schmeier@gmail.com>
2016-03-03 17:15:00 +1300

    corected error in sicke files.

--- run_sickle_se.sh
+++ run_sickle_se.sh
@@ -8,13 +8,13 @@ now=$(date +"%Y-%d-%m_%H%M%S")
 errfile=${outdir}/sickle_se.${now}.stderr;
 outfile=${outdir}/sickle_se.${now}.stdout;
 
-$outdir_2=${outdir_pre}/sickle_se.${now}
-mkdir ${outdir_2}
+outdir2=${outdir}/sickle_se.${now};
+mkdir ${outdir2};
 
 for i in `ls ${dir}/*fastq*`; do
     echo ${i} >> ${errfile};
     echo ${i} >> ${outfile};
-    sickle se -f ${i} -t sanger -g -o ${outdir_2}/$(basename ${i} | sed 's/.fastq.gz/.fastq.trimmed.gz/g') 2>> ${errfile} >> ${outfile};
+    sickle se -f ${i} -t sanger -g -o "${outdir2}/$(basename ${i} | sed 's/.fastq.gz/.fastq.trimmed.gz/g')" 2>> ${errfile} >> ${outfile};
     echo "-------------" >> ${errfile};
     echo "-------------" >> ${outfile};
 done;
