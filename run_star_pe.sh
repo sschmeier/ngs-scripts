db9a83cc06415263e37b683943cf0477ad2bad12
Sebastian Schmeier <s.schmeier@gmail.com>
2016-03-04 13:32:47 +1300

    removed path in star

--- run_star_pe.sh
+++ run_star_pe.sh
@@ -15,17 +15,17 @@ outdir_2=${outdir}/star_pe.${now};
 mkdir ${outdir_2};
 
 # load genome into mem
-/mnt/DATA1/seb/bin/STAR_2.5.1b/STAR --runThreadN 2 --genomeLoad LoadAndExit --genomeDir ${genomeindex} 2>> ${errfile} >> ${outfile};
+STAR --runThreadN 2 --genomeLoad LoadAndExit --genomeDir ${genomeindex} 2>> ${errfile} >> ${outfile};
 
 for i in `ls ${dir}/*_1.fastq.trimmed.gz`; do
     echo ${i} >> ${errfile};
     echo ${i} >> ${outfile};
 
-    /mnt/DATA1/seb/bin/STAR_2.5.1b/STAR --outFilterMultimapNmax 1 --outSAMtype BAM SortedByCoordinate --runThreadN 2 --alignIntronMax 1 --alignEndsType EndToEnd --genomeDir ${genomeindex} --readFilesCommand zcat --readFilesIn ${i} $(echo ${i} | sed 's/_1/_2/g') --outFileNamePrefix ${outdir_2}/$(basename ${i} | sed 's/_1.fastq.trimmed.gz//g') 2>> ${errfile} >> ${outfile};
+    STAR --outFilterMultimapNmax 1 --outSAMtype BAM SortedByCoordinate --runThreadN 2 --alignIntronMax 1 --alignEndsType EndToEnd --genomeDir ${genomeindex} --readFilesCommand zcat --readFilesIn ${i} $(echo ${i} | sed 's/_1/_2/g') --outFileNamePrefix ${outdir_2}/$(basename ${i} | sed 's/_1.fastq.trimmed.gz//g') 2>> ${errfile} >> ${outfile};
 
     echo "----------" >> ${errfile};
     echo "----------" >> ${outfile};
 done;
  
-/mnt/DATA1/seb/bin/STAR_2.5.1b/STAR --runThreadN 2 --genomeLoad Remove --genomeDir ${genomeindex} 2>> ${errfile} >> ${outfile};
+STAR --runThreadN 2 --genomeLoad Remove --genomeDir ${genomeindex} 2>> ${errfile} >> ${outfile};
 
--- run_star_se.sh
+++ run_star_se.sh
@@ -15,17 +15,17 @@ outdir_2=${outdir}/star_se.${now};
 mkdir ${outdir_2};
 
 # load genome into mem
-/mnt/DATA1/seb/bin/STAR_2.5.1b/STAR --runThreadN 2 --genomeLoad LoadAndExit --genomeDir ${genomeindex} 2>> ${errfile} >> ${outfile};
+STAR --outFileNamePrefix ${outdir_2} --runThreadN 2 --genomeLoad LoadAndExit --genomeDir ${genomeindex} 2>> ${errfile} >> ${outfile};
 
 for i in `ls ${dir}/*.fastq.trimmed.gz*`; do
     echo ${i} >> ${errfile};
     echo ${i} >> ${outfile};
 
-    /mnt/DATA1/seb/bin/STAR_2.5.1b/STAR --outFilterMultimapNmax 1 --outSAMtype BAM SortedByCoordinate --runThreadN 2 --alignIntronMax 1 --alignEndsType EndToEnd --genomeDir ${genomeindex} --readFilesCommand zcat --readFilesIn ${i} --outFileNamePrefix ${outdir_2}/$(basename ${i} | sed 's/.fastq.trimmed.gz//g' ) 2>> ${errfile} >> ${outfile}; 
+    STAR --outFilterMultimapNmax 1 --outSAMtype BAM SortedByCoordinate --runThreadN 2 --alignIntronMax 1 --alignEndsType EndToEnd --genomeDir ${genomeindex} --readFilesCommand zcat --readFilesIn ${i} --outFileNamePrefix ${outdir_2}/$(basename ${i} | sed 's/.fastq.trimmed.gz//g') 2>> ${errfile} >> ${outfile}; 
 
     echo "----------" >> ${errfile};
     echo "----------" >> ${outfile};
 done;
  
-/mnt/DATA1/seb/bin/STAR_2.5.1b/STAR --runThreadN 2 --genomeLoad Remove --genomeDir ${genomeindex} 2>> ${errfile} >> ${outfile};
+STAR --outFileNamePrefix ${outdir_2} --runThreadN 2 --genomeLoad Remove --genomeDir ${genomeindex} 2>> ${errfile} >> ${outfile};
 
