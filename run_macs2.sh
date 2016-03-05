c207f23feff7306930744b022f78c21a2687a0ab
Sebastian Schmeier <s.schmeier@gmail.com>
2016-03-06 07:31:53 +1300

    added macs2

---
+++ run_macs2.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+# USAGE: script.sh in-dir out-dir genomesize
+
+dir=$1;
+outdir=$2;
+genomesize=$3;
+
+now=$(date +"%Y-%m-%d_%H%M%S")
+
+# set error and stdout file
+errfile=${outdir}/macs2.${now}.stderr
+outfile=${outdir}/macs2.${now}.stdout
+
+outdir_2=${outdir}/macs2.${now};
+mkdir ${outdir_2};
+
+for i in `ls ${dir}/*.bam`; do
+
+    echo ${i} >> ${errfile};
+    echo ${i} >> ${outfile};
+    
+    macs2 callpeak -n ${outdir_2}/$(basename ${i} | sed 's/.bam//g') \
+                   -t ${i} \
+                   -f BAM \
+                   -g ${genomesize} \
+                   -q 0.05 \
+                   2>> ${errfile} >> ${outfile}; 
+
+    echo "----------" >> ${errfile};
+    echo "----------" >> ${outfile};
+done;
+ 
