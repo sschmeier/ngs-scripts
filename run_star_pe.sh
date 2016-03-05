d423091dee1b895d0059cd639990c2bfc6df7eea
Sebastian Schmeier <s.schmeier@gmail.com>
2016-03-05 13:22:28 +1300

    STAR now with shared genomes

--- run_star_pe.sh
+++ run_star_pe.sh
@@ -15,17 +15,36 @@ outdir_2=${outdir}/star_pe.${now};
 mkdir ${outdir_2};
 
 # load genome into mem
-STAR --runThreadN 2 --genomeLoad LoadAndExit --genomeDir ${genomeindex} 2>> ${errfile} >> ${outfile};
+STAR --outFileNamePrefix ${outdir_2} \
+     --runThreadN 2 \
+     --genomeLoad LoadAndExit \
+     --genomeDir ${genomeindex} \
+     2>> ${errfile} >> ${outfile};
 
 for i in `ls ${dir}/*_1.fastq.trimmed.gz`; do
     echo ${i} >> ${errfile};
     echo ${i} >> ${outfile};
 
-    STAR --outFilterMultimapNmax 1 --outSAMtype BAM SortedByCoordinate --runThreadN 2 --alignIntronMax 1 --alignEndsType EndToEnd --genomeDir ${genomeindex} --readFilesCommand zcat --readFilesIn ${i} $(echo ${i} | sed 's/_1/_2/g') --outFileNamePrefix ${outdir_2}/$(basename ${i} | sed 's/_1.fastq.trimmed.gz//g') 2>> ${errfile} >> ${outfile};
+    nice STAR --readFilesIn ${i} $(echo ${i} | sed 's/_1/_2/g') \
+         --outFileNamePrefix ${outdir_2}/$(basename ${i} | sed 's/_1.fastq.trimmed.gz//g') \
+         --limitBAMsortRAM 20000000000 \
+         --genomeLoad LoadAndKeep \
+         --outFilterMultimapNmax 1 \
+         --outSAMtype BAM SortedByCoordinate \
+         --runThreadN 2 \
+         --alignIntronMax 1 \
+         --alignEndsType EndToEnd \
+         --genomeDir ${genomeindex} \
+         --readFilesCommand zcat \
+         2>> ${errfile} >> ${outfile};         
 
     echo "----------" >> ${errfile};
     echo "----------" >> ${outfile};
 done;
  
-STAR --runThreadN 2 --genomeLoad Remove --genomeDir ${genomeindex} 2>> ${errfile} >> ${outfile};
+STAR --outFileNamePrefix ${outdir_2} \
+     --runThreadN 2 \
+     --genomeLoad Remove \
+     --genomeDir ${genomeindex} \
+     2>> ${errfile} >> ${outfile};
 
--- run_star_se.sh
+++ run_star_se.sh
@@ -15,17 +15,38 @@ outdir_2=${outdir}/star_se.${now};
 mkdir ${outdir_2};
 
 # load genome into mem
-STAR --outFileNamePrefix ${outdir_2} --runThreadN 2 --genomeLoad LoadAndExit --genomeDir ${genomeindex} 2>> ${errfile} >> ${outfile};
+# MAKES ONLY SENSE IF --limitBAMsortRAM IS SET IN SUBSEQUENT RUNS
+STAR --outFileNamePrefix ${outdir_2} \
+     --runThreadN 2 \
+     --genomeLoad LoadAndExit \
+     --genomeDir ${genomeindex} \
+     2>> ${errfile} >> ${outfile};
 
 for i in `ls ${dir}/*.fastq.trimmed.gz*`; do
     echo ${i} >> ${errfile};
     echo ${i} >> ${outfile};
-
-    STAR --outFilterMultimapNmax 1 --outSAMtype BAM SortedByCoordinate --runThreadN 2 --alignIntronMax 1 --alignEndsType EndToEnd --genomeDir ${genomeindex} --readFilesCommand zcat --readFilesIn ${i} --outFileNamePrefix ${outdir_2}/$(basename ${i} | sed 's/.fastq.trimmed.gz//g') 2>> ${errfile} >> ${outfile}; 
+    
+    # --limitBAMsortRAM NEEDS TO BE SET IF SHARED GENOME SHOULD BE USED
+    nice STAR --outFileNamePrefix ${outdir_2}/$(basename ${i} | sed 's/.fastq.trimmed.gz//g') \
+         --limitBAMsortRAM 20000000000 \
+         --genomeLoad LoadAndKeep \
+         --outFilterMultimapNmax 1 \
+         --outSAMtype BAM SortedByCoordinate \
+         --runThreadN 2 \
+         --alignIntronMax 1 \
+         --alignEndsType EndToEnd \
+         --genomeDir ${genomeindex} \
+         --readFilesCommand zcat \
+         --readFilesIn ${i} \
+         2>> ${errfile} >> ${outfile}; 
 
     echo "----------" >> ${errfile};
     echo "----------" >> ${outfile};
 done;
  
-STAR --outFileNamePrefix ${outdir_2} --runThreadN 2 --genomeLoad Remove --genomeDir ${genomeindex} 2>> ${errfile} >> ${outfile};
+STAR --outFileNamePrefix ${outdir_2} \
+     --runThreadN 2 \
+     --genomeLoad Remove \
+     --genomeDir ${genomeindex} \
+     2>> ${errfile} >> ${outfile};
 
